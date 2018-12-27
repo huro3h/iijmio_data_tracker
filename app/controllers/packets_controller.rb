b58299dce5723e3d269445abddb70bcaa66064b7
huro3h <huro3h@gmail.com>
2018-12-28 07:44:11 +0900

    Update packets controller

--- app/controllers/packets_controller.rb
+++ app/controllers/packets_controller.rb
@@ -2,16 +2,7 @@
 
 class PacketsController < ApplicationController
   def index
-    if Packet.exist_past30days_data?
-      shape_data = Iijmio.generate_packet_data
-      shape_data.each do |daily_data|
-        packet = Packet.new(packet_params)
-        packet.amount = daily_data.amount
-        packet.date = daily_data.date
-        packet.save
-      end
-    end
-    @packets = Packet.where.not(id: nil).limit(10)
+    # @packets = Packet.last
   end
 
   private
