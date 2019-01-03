99a8abfcf2d3ce36094efdd89357d89ae50674c1
huro3h <huro3h@gmail.com>
2019-01-03 11:47:49 +0900

    Fix SyncPacket#execute

--- app/models/sync_packet.rb
+++ app/models/sync_packet.rb
@@ -2,12 +2,12 @@
 
 class SyncPacket
   def execute
-    unless Packet.is_latest?
+    unless Packet.latest?
       shape_data = Iijmio.generate_packet_data
       shape_data.each do |daily_data|
-        packet = Packet.new(packet_params)
+        packet = Packet.new
         packet.amount = daily_data.amount
-        packet.date = daily_data.date
+        packet.used_at = daily_data.date
         packet.save
       end
     end
