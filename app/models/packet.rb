58e55c66aaeaf98650d7b4e807c9101859ec2c54
huro3h <huro3h@gmail.com>
2019-01-03 15:25:04 +0900

    Change method name to not_latest?

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -3,7 +3,7 @@
 class Packet < ApplicationRecord
   validates :used_at, uniqueness: true
 
-  def self.latest?
-    Date.yesterday == self.last.used_at.to_date
+  def self.not_latest?
+    Date.yesterday != self.order(used_at: :desc).first.used_at
   end
 end
