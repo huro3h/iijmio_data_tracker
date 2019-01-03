9a4a4995f5100b57a6d838d00978977defddaf04
huro3h <huro3h@gmail.com>
2019-01-03 15:42:08 +0900

    Add scope last_month

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -4,6 +4,7 @@ class Packet < ApplicationRecord
   validates :used_at, uniqueness: true
 
   scope :current_month, -> { where(used_at: Time.now.beginning_of_month..Time.now.end_of_month) }
+  scope :last_month, -> { where(used_at: Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month) }
   def self.not_latest?
     Date.yesterday != self.order(used_at: :desc).first.used_at
   end
