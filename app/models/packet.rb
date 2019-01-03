2e26c90df343a35dfe35f80d060006a76633c6d5
huro3h <huro3h@gmail.com>
2019-01-03 15:42:43 +0900

    Add scope total_amount

--- app/models/packet.rb
+++ app/models/packet.rb
@@ -5,6 +5,9 @@ class Packet < ApplicationRecord
 
   scope :current_month, -> { where(used_at: Time.now.beginning_of_month..Time.now.end_of_month) }
   scope :last_month, -> { where(used_at: Time.now.prev_month.beginning_of_month..Time.now.prev_month.end_of_month) }
+
+  scope :total_amount, -> { pluck(:amount).sum }
+
   def self.not_latest?
     Date.yesterday != self.order(used_at: :desc).first.used_at
   end
