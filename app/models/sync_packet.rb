ef7190d5097fb694725e32ff5bd7fcc72c0688eb
huro3h <huro3h@gmail.com>
2019-01-07 22:37:04 +0900

    Update SyncPacket#execute

--- app/models/sync_packet.rb
+++ app/models/sync_packet.rb
@@ -5,9 +5,7 @@ class SyncPacket
     if (Packet.exists? && Packet.not_latest?) || Packet.count.zero?
       shape_data = Iijmio.generate_packet_data
       shape_data.each do |daily_data|
-        packet = Packet.new
-        packet.amount = daily_data.amount
-        packet.used_at = daily_data.date
+        packet = Packet.new(amount: daily_data.amount, used_at: daily_data.date)
         packet.save
       end
     end
